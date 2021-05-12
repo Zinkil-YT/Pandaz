e1788cd347b063fc09679ce5e4ff14cb6756f34e
xVaxyr <83889741+xVaxyr@users.noreply.github.com>
2021-05-12 12:59:14 +0200

    Add files via upload

---
+++ src/Zinkil/pc/listeners/DistanceListener.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zinkil\pc\listeners;
+
+use pocketmine\event\Listener;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\item\Item;
+use pocketmine\level\Location;
+use pocketmine\math\Vector3;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use Zinkil\pc\Core;
+use Zinkil\pc\CPlayer;
+use Zinkil\pc\Utils;
+use Zinkil\pc\managers\PlayerProtectManager as PPM;
+
+class DistanceListener implements Listener{
+	
+	public $plugin;
+	
+	public $combo;
+	
+	public function __construct(){
+		$this->plugin=Core::getInstance();
+		$this->combo = new Config($this->plugin->getDataFolder() . "combo.yml", Config::YAML);
+	}
+	/**
+	* @priority LOW
+	*/
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        /* Register Player */
+        $this->combo->set($player->getName(), 0);
+    }
+    
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $player = $event->getPlayer();
+        /* Register Player */
+        if (PPM::isIn($player)) {
+            PPM::delIn($player);
+        }
+    }
+    
+    public function onPlayerDeath(PlayerDeathEvent $event)
+    {
+    	$player = $event->getPlayer();
+    	/* If player has more than 0 combos */
+        if ($this->combo->get($player->getName()) >= 1)
+        {
+            /* Reset combo on quit */
+            $this->combo->set($player->getName(), 0);
+        }
+        if (PPM::isIn($player)) {
+            PPM::delIn($player);
+        }
+    }
+	/**
+	* @priority HIGH
+	*/
+	public function onEntityDamageByEntity(EntityDamageEvent $event){
+		$player=$event->getEntity();
+		$cause=$event->getCause();
+		$level=$player->getLevel()->getName();
+		switch($cause){
+			case EntityDamageEvent::CAUSE_ENTITY_ATTACK:
+			$damager=$event->getDamager();
+			$dlevel=$damager->getLevel()->getName();
+			if(!$player instanceof Player) return;
+			if(!$damager instanceof Player) return;
+			if($damager->isCreative()) return;
+			if($level==Core::LOBBY) return;
+			if($dlevel==Core::LOBBY) return;
+			if($event->isCancelled()) return; // Check if hit from safezone/attack cooldown
+			$damagerpos=$damager->getPosition() ?? new Vector3(0,0,0);
+			$playerpos=$player->getPosition() ?? new Vector3(0,0,0);
+			$distance=$damagerpos->distance($playerpos);
+			$health=round($player->getHealth(), 1);
+			$playername=$player->getDisplayName();
+			$damagername=$damager->getDisplayName();
+			$this->combo->set($damager->getName(), $this->combo->get($damager->getName()) + 1);
+			$this->combo->set($player->getName(), 0);
+			$player->sendPopup("§e".$damagername."§e: §7".$distance);
+			$damager->sendPopup("§r§bDistance: §7".$distance." §7| §bCombo: §7".$this->combo->get($damager->getName()));
+            if ($player instanceof Player && $damager instanceof Player) {
+                if (PPM::isIn($player)) {
+                    if (PPM::getIn($player) === $damager->getName()) {
+                        PPM::setIn($player, $damager);
+                    } else {
+                        $event->setCancelled();
+                    }
+                } elseif (PPM::isIn($damager)) {
+                    if (PPM::getIn($damager) === $player->getName()) {
+                        PPM::setIn($player, $damager);
+                    } else {
+                        $event->setCancelled();
+                    }
+                } else {
+                    PPM::setIn($player, $damager);
+                }
+            }
+		}
+	}
+}
