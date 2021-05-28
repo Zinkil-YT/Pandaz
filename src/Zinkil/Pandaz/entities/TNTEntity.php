f28f17a15f32b569d73b633e832cb9b8f3cef435
Zinkil-YT <74324305+Zinkil-YT@users.noreply.github.com>
2021-05-28 17:34:10 +0200

    Add files via upload

---
+++ src/Zinkil/Pandaz/entities/TNTEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zinkil\Pandaz\entities;
+
+use pocketmine\entity\object\PrimedTNT;
+use pocketmine\entity\Entity;
+use Zinkil\Pandaz\Core;
+
+class TNTEntity extends PrimedTNT{
+
+    public function entityBaseTick(int $tickDiff=1):bool{
+        $this->setNameTag("§l§4Time: ".$this->fuse);
+        $this->setNameTagVisible();
+        $this->setNameTagAlwaysVisible();
+        return parent::entityBaseTick($tickDiff);
+    }
+}
